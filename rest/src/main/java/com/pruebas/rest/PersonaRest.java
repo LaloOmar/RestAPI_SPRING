a5c12476fbab88a66499fda89dba7c29b68992ef
LaloOmar <ing.eduardo@live.com.mx>
2021-02-22 01:18:45 -0600

    add put and delete

--- rest/src/main/java/com/pruebas/rest/PersonaRest.java
+++ rest/src/main/java/com/pruebas/rest/PersonaRest.java
@@ -1,8 +1,11 @@
 package com.pruebas.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +28,16 @@ public class PersonaRest {
 	public java.util.List<Persona> listar() {
 		return personaDAO.findAll();
 	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminar(@PathVariable("id") Integer id) {
+		personaDAO.deleteById(id);
+	}
 
+	@PutMapping("/actualizar")
+	public void actualizar(@RequestBody Persona persona) {
+		personaDAO.save(persona);
+	}
+	
+	
 }
